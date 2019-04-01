e63d4079a77f2dc9ce97197f1ecce34bc2f85238
Faris Zahrah <>
2019-04-01 09:50:21 -0500

    gonna kms, cant find this bug

--- phase_3/index.py
+++ phase_3/index.py
@@ -9,6 +9,9 @@ from sklearn.preprocessing import normalize
 from collections import Counter
 from numpy import dot
 import numpy as np
+from numpy.linalg import norm
+
+
 class index:
         def __init__(self,path):
 
@@ -59,9 +62,11 @@ class index:
                 # set the docID in doc_to_term
                 self.doc_to_term[i] = {}
 
-                pattern = re.compile('\w+(?:-\w+)+|\w+[^ ]\w+|\w+',re.IGNORECASE)
+                #pattern = re.compile('\w+(?:-\w+)+|\w+[^ ]\w+|\w+',re.IGNORECASE)
+                pattern = re.compile('\w+',re.IGNORECASE)
+        
                 tokens = re.findall(pattern, docs[i][27:])
-                
+                #tokens = re.split('[\W]', docs[i][27:])
                 #now we populate doc_to_term, term_to_termID
                 for token in tokens:
                     if token not in self.stop_words:
@@ -82,19 +87,18 @@ class index:
                 
                 for termID in self.doc_to_term[i]:
                     if termID not in self.posting_list:
-                        self.posting_list[termID] = [math.log10(len(docs)),(i,self.doc_to_term[i][termID])]
+                        tfid = math.log10(len(docs))
+                        self.posting_list[termID] = [tfid,(i,self.doc_to_term[i][termID])]
                     else:
+                        self.posting_list[termID][0]= math.log10(len(docs)/len(self.posting_list[termID]))
                         self.posting_list[termID].append((i,self.doc_to_term[i][termID]))
-                        self.posting_list[termID][0]= math.log10(len(docs)/(len(self.posting_list[termID])-1))
 
             self.termID_to_term = dict([[v,k] for k,v in self.term_to_termID.items()])
             print('Index built in {0}'.format(time.time()-start_time))
 
-
         # exact top-k search
         # return top-k docIDs and their scores
         def exact_search(self, query, k):
-            query = self.filter_query(query)
 
             docs_to_score = self.find_all_docs(query) 
             doc_scores = self.score_docs(query, docs_to_score)
@@ -120,34 +124,39 @@ class index:
                 print('YOUR FEEDBACK IS GARBAGE MATE\n I need documentIDs to proceed')
                 return
 
-            query = self.filter_query(query)
             # first i just compute the vocab
             # this may not bbe the fastest way
             # i will refine after a working program
             vocab, q_vector, pos_feedback_vectors, neg_feedback_vectors = self.create_rocchio_vectors(query, 
                                                                             pos_feedback, neg_feedback)
-
+            
+            print('length of pos feedback: {0}, Length of negative feedback: {1}'.format(len(pos_feedback),len(neg_feedback)))
             final_vector = q_vector 
             pos_vector_sum = self.sum_vector_list(list(pos_feedback_vectors.values()))
             neg_vector_sum = self.sum_vector_list(list(neg_feedback_vectors.values()))
             
-            #weight vectors
-            pos_vector_sum = list(np.array(pos_vector_sum) * beta)
-            neg_vector_sum = list(np.array(neg_vector_sum) * gamma * -1)
-            
+            # weight vectors
+            # and multiply by the number of documents 
+            pos_vector_sum = list(np.array(pos_vector_sum) * beta / len(pos_feedback))
+            neg_vector_sum = list(np.array(neg_vector_sum) * gamma * -1 / len(neg_feedback))
             final_vector = self.sum_vector_list([final_vector, pos_vector_sum, neg_vector_sum]) 
                 
             weighted_d = {}
+            vector_weighted = {}
             for index,val in enumerate(vocab):
-                weighted_d[val] = final_vector[index]
-
-            return vocab,final_vector, weighted_d 
+                if final_vector[index] > 0:
+                    vector_weighted[val] = final_vector[index]
+                    weighted_d[self.termID_to_term[val]] = final_vector[index]
+            
+            
+            return vocab,vector_weighted, weighted_d 
        
 
 
 
         # returns all the vectors to perform rocchio  
         def create_rocchio_vectors(self, query, pos_feedback, neg_feedback):
+            
             vocab = set()
             for doc in pos_feedback:
                 vocab |= set(self.doc_to_term[doc].keys())
@@ -188,6 +197,25 @@ class index:
 
             return vocab, q_vector, pos_doc_vectors, neg_doc_vectors
 
+        '''
+        def roc_2(self, query, pos_f, neg_f, alpha = 1, beta = 0.75, gamma = 0.15):
+
+            # create vocab
+            vocab = set()
+            for i in pos_f+neg_f:
+                vocab |= set(self.doc_to_term[i].keys())
+            vocab |= set(query.keys())
+            
+            pos_vectors = [] #list of lists, each sub list is a doc vector
+            neg_vectors = []
+            
+            for doc in pos_f:
+                v = []
+                for term in vocab:
+                    if term in self.doc_to_term[doc]:
+                        v.append(
+        '''
+
 
 
         #function to print the terms and posting list in the index 
@@ -210,6 +238,7 @@ class index:
         #take raw query(string), parse and return a counter object of termID:frequency
         def filter_query(self, query):
             pattern = re.compile('\w+(?:-\w+)+|\w+[^ ]\w+|\w+',re.IGNORECASE)
+           # pattern = re.compile('\w+',re.IGNORECASE)
             tokens = re.findall(pattern, query.lower())
             #tokens = query.lower().split()
             query_ret=[]
@@ -249,29 +278,31 @@ class index:
         def compute_cosine(self, query, docID):
             query_vector = []
             doc_vector = []
-            query_copy = query.copy()
             for index, i in enumerate(self.doc_to_term[docID]):
                 TFIDF = (1+math.log10(self.doc_to_term[docID][i])) * self.posting_list[i][0]
                 doc_vector.append(TFIDF)
 
                 if i in query:
                     TFIDF = (self.posting_list[i][0])
-                    query_vector.append(query_copy[i] * TFIDF)
-                    del query_copy[i]
+                    query_vector.append(query[i] * TFIDF)
                 else:
                     query_vector.append(0)
 
-            for i in query_copy:
-                TFIDF = (self.posting_list[i][0])
-                query_vector.append(query_copy[i] * (self.posting_list[i][0]))
-                doc_vector.append(0)
+            for i in query:
+                if i not in self.doc_to_term[docID]:
+                    TFIDF = (self.posting_list[i][0])
+                    query_vector.append(query[i] * (self.posting_list[i][0]))
+                    doc_vector.append(0)
 
             # normalize and dot product
-            query_vector = normalize([query_vector])[0]
-            doc_vector = normalize([doc_vector])[0]
+            #query_vector = normalize([query_vector])[0]
+            #doc_vector = normalize([doc_vector])[0]
 
             #score = 1 - spatial.distance.cosine(query_vector, doc_vector)
-            score = np.array(query_vector).dot(np.array(doc_vector))
+            #score = np.array(query_vector).dot(np.array(doc_vector))
+            
+            score = dot(query_vector, doc_vector)/(norm(query_vector)*norm(doc_vector))
+            
             return score 
             
             
@@ -313,23 +344,28 @@ def main():
         if i.posting_list[x][0] < 0:
             print(i.posting_list[x])
     
-    query ='BACKGROUND OF THE NEW CHANCELLOR OF WEST GERMANY, LUDWIG ERHARD'
-    docs = i.exact_search( query ,10)
-    
-    # print results
-    for z in docs:
-        print(z, i.doc_to_docID[z[1]])
-   
+    query = ' BACKGROUND OF THE NEW CHANCELLOR OF WEST GERMANY, LUDWIG ERHARD . '
+    query = i.filter_query(query)
     inp = 'y'
+   
     while inp == ('y' or 'Y'):
+        docs = i.exact_search(query ,10)
+       
+        # print results
+        for z in docs:
+            print(z, i.doc_to_docID[z[1]])
+   
+
+        
         inp = input('\nWould you like to proceed with Rocchio: (y/n): ')
         if inp == ('n' or 'N'):
             print('okay, I guess we are done here :)')
         elif inp ==('y' or 'Y'):
             pos_feedback = input('\nEnter relevant document ids separated by space: ').split()
             neg_feedback = input('\nEnter non relevant document ids seperated bs space: ').split() 
-            vocab, new_q = i.rocchio(query, pos_feedback, neg_feedback)
-
+            vocab, query, weighted_d = i.rocchio(query, pos_feedback, neg_feedback)
+            
+            print('len of weighted dict: {0}'.format(len(weighted_d)))
         else:
             print("\nEnter 'y/n' idiot, I didnt give you any other options!")
             inp = 'y'
